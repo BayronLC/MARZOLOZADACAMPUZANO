a339e0bd6d7af8257cc7f92d377c1406782bcc89
dam <dam@10.100.100.41>
2021-03-09 09:13:27 +0100

    segunda version

---
+++ src/marzo/NOMBRE.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marzo;
+
+/**
+ *
+ * @author dam
+ */
+public class NOMBRE {
+    
+    private String dato3;
+    
+    private String dato4;
+    
+}
